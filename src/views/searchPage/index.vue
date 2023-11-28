956ea1ad8e5db82e959f0101e6d31f4e37258e20
GJXZK <2587579585@qq.com>
2023-11-28 12:31:56 +0800

    feat：修改搜索结果表格的样式

--- src/views/searchPage/index.vue
+++ src/views/searchPage/index.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import useSearchKeywords from '@/stores/homePage'
 import type { SearchSongsDto } from '@/model/search'
 const SearchKeywords = useSearchKeywords()
 import searchApi from '@/api/Search/search'
 import type { Ref } from 'vue'
+import {useGlobalStore} from '@/stores/global'
+const globalStore = useGlobalStore()
+const searchPageHeight = computed(()=>{
+  return {
+    height:`${globalStore.windowHeight}px`
+  }
+})
 // 展示在表格里的数据
 const SearchRequest: Ref<SearchSongsDto[]> = ref([])
 // 搜索歌曲
 async function getSearchSongByKeyword(keyword: string) {
   console.log(keyword)
   let resp = await searchApi.goSearch(keyword)
-  console.log(resp)
   getShowSearchSongs(resp)
 }
 // 将搜索结果转换为可以在表格中使用的数据
@@ -43,27 +49,64 @@ watch(
 const columns = [
   {
     title: '音乐标题',
-    dataIndex: 'name'
+    dataIndex: 'name',
+    slotName: 'name'
   },
   {
     title: '歌手',
-    dataIndex: 'singer'
+    dataIndex: 'singer',
+    slotName: 'singer'
   },
   {
     title: '专辑',
-    dataIndex: 'album'
+    dataIndex: 'album',
+    slotName: 'album'
+
   }
 ]
+const pagination={pageSize:30}
 </script>
 <template>
-  <div class="h-full">
+  <div class="h-full ">
     <div>
       <!-- 搜索关键词 -->
       <div class="ml-20px">
-        <h1>搜索：{{ SearchKeywords.searchKeyword }}</h1>
+        <h1>搜索：{{ SearchKeywords.searchKeyword }}{{ globalStore.windowHeight }}</h1>
       </div>
-      <div class="p-20px">
-        <a-table :columns="columns" :data="SearchRequest" :bordered="false"> </a-table>
+      <div class="p-20px" >
+        <a-table 
+          :columns="columns" 
+          :data="SearchRequest" 
+          :bordered="false" 
+          :pagination="pagination" 
+          pageSize="30"
+          :virtual-list-props="{searchPageHeight}"
+        >
+          <!-- 音乐标题 -->
+          <template #name="{ record }">
+            <div class="flex items-center cursor-pointer">
+              <span @click="">
+                {{ record.name }}
+              </span>
+            </div>
+          </template>
+          <!-- 歌手 -->
+          <template #singer="{ record }">
+            <div class="flex items-center cursor-pointer">
+              <span @click="">
+                {{ record.singer }}
+              </span>
+            </div>
+          </template>
+          <!-- 专辑 -->
+          <template #album="{ record }">
+            <div class="flex items-center cursor-pointer">
+              <span @click="">
+                {{ record.album }}
+              </span>
+            </div>
+          </template>
+        </a-table>
       </div>
     </div>
   </div>
